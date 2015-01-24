33e12a9d1c9531f2e29c8fd5f4f5601a260cbc5c
salazarmiguel12 <salazarmiguel12@gmail.com>
2015-01-24 03:37:14 -0800

    Update grepdir.sh

--- p2/grepdir.sh
+++ p2/grepdir.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
+#Author Miguel Salazar
+
+if [[ $# -lt 2 ]]; then
+    echo "usage: $0 directory pattern [-grep option]*"
+  exit 1 #exits with failure of 1 because it does not reach the second peramitter.
+fi   
 
 
+if [[ $# -eq 2 ]]; then
+    find $1 -exec grep $2 {} \; 
+else
+    if [[ $# -eq 3 ]]; then
+      find $1 -exec grep $3 $2 {} \;
+    else  
+      if [[ $# -eq 4 ]]; then
+        find $1 -exec grep $4 $3 $2 {} \;
+    fi 
+  fi 
+fi 
 
-if [[ "$1" = "." ]]; then
-	if [[ "$2" = "bin" ]]; then 
-		grep -l /bin * |  wc -l
-		echo "bin in file"
-	fi
-	
-else  
-	echo "usage: grepdir.sh directory pattern [-grep option]*"
-fi
+#need to finish
